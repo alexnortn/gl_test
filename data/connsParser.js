6624393f965c86e835bce638e9445a222df65f8a
Alex Norton <alexnortn@gmail.com>
2017-02-27 15:09:02 -0500

    transformed individual cell output json

--- data/connsParser.js
+++ data/connsParser.js
@@ -1,13 +1,11 @@
 // 2017 Alex Norton
 // Neural reconstruction data (e2198) parser (csv -> json)
 
-// * To Do
-// * Test values with scaling transform [16, 16, 22.5]
+// * Run from Node
 
 'use strict';
 
 let fs = require('fs');
-let csv = require('csv');
 let jsonfile = require('jsonfile');
 
 let connsIn = fs.readFileSync('conns.csv', 'utf8');
@@ -18,44 +16,44 @@ let transform = { "x": 16, "y": 16, "z": 22.5 };
 // Assumse conns -> conns ~ [14] ~ [0] -> length
 // Returns Map type 
 function parseConns(conns) {
-	let temp = [];
+	let obj;
 	let map = new Map();
 
 	// Parse array(String) -> Array(Array)
 	conns = conns.map((item) => item.split(','));
 
 	conns[0].forEach((item, index) => { 
-		temp = {
-			"id":    conns[1][index],
+		obj = {
+			"id":    		  conns[1][index],
 			"area": {
-				"x": transform.x * parseInt(conns[2][index]),
-				"y": transform.y * parseInt(conns[3][index]),
-				"z": transform.z * parseInt(conns[4][index])
+				"x": parseInt(conns[2][index])  * transform.x,
+				"y": parseInt(conns[3][index])  * transform.y,
+				"z": parseInt(conns[4][index])  * transform.z
 			},
 			"centroid": {
-				"x": transform.x * parseInt(conns[5][index]),
-				"y": transform.y * parseInt(conns[6][index]),
-				"z": transform.z * parseInt(conns[7][index])
+				"x": parseInt(conns[5][index])  * transform.x,
+				"y": parseInt(conns[6][index])  * transform.y,
+				"z": parseInt(conns[7][index])  * transform.z
 			},
 			"post": {
-				"x": transform.x * parseInt(conns[8][index]),
-				"y": transform.y * parseInt(conns[9][index]),
-				"z": transform.z * parseInt(conns[10][index])
+				"x": parseInt(conns[8][index])  * transform.x,
+				"y": parseInt(conns[9][index])  * transform.y,
+				"z": parseInt(conns[10][index]) * transform.z
 			},
 			"pre": {
-				"x": transform.x * parseInt(conns[11][index]),
-				"y": transform.y * parseInt(conns[12][index]),
-				"z": transform.z * parseInt(conns[13][index])
+				"x": parseInt(conns[11][index]) * transform.x,
+				"y": parseInt(conns[12][index]) * transform.y,
+				"z": parseInt(conns[13][index]) * transform.z
 			}
 		};
 
 		if (map.has(item)) {
-			let mapTemp = map.get(item);
-				mapTemp.push(temp);
-			map.set(item, mapTemp);
+			let temp = map.get(item);
+				temp.push(obj);
+			map.set(item, temp);
 		}
 		else {
-			map.set(item, [temp]);
+			map.set(item, [obj]);
 		}
 
 	});
@@ -63,25 +61,59 @@ function parseConns(conns) {
 	return map;
 }
 
+
 // Output cell contacts -> json
 new Promise(function(resolve, reject){
     resolve(parseConns(conns)); // parse conns.csv (expect Map)
 })
 .then(function(data){
 	let obj = {};
-	let file;
-	function mapToObj(value, key) { 
-		obj[key] = value; // Set up object for complete export
-		file = "parsedDataTransform/conns-" + key + ".json"; // Individual cell export
-		jsonfile.writeFile(file, value, (error) => { if (error) console.error(err) });
-	}
-
-	data.forEach(mapToObj); // Iterate through conns Map
 	
-	// Write all cell contacts to json
-	file = 'parsedDataTransform/conns.json'; 
-	jsonfile.writeFile(file, obj, (error) => { if (error) console.error(err) });
+	// Iterate through conns Map, setup <obj> for complete export
+	data.forEach((value, key) => obj[key] = value);
+	
+	return obj;
+
+})
+.then(function(data) {
+	let file;
+	// Write all cell + contacts to json
+	file = 'parsedDataTransform2/conns.json'; 
+	jsonfile.writeFile(file, data, (error) => { if (error) console.error(err) });
+
+	// Get cell by id, get contacts with specific cell
+	// contacts = conns['cell1'].filter((el) => { if (el.id === 'cell2') return el });
+
+	return data;
+
+})
+.then(function(data) {
+	let file, obj;
+	let map = new Map();
+	// Write each cell contacts to json
+	Object.entries(data).forEach(([cellName, cell]) => {
+		map.clear();
+		cell.forEach((contact) => {
+			if (map.has(contact.id)) {
+				let temp = map.get(contact.id);
+					temp.push(contact);
+				map.set(contact.id, temp);
+			}
+			else {
+				map.set(contact.id, [contact]);
+			}
+		});
+		
+		obj = {};
+		map.forEach((value, key) => obj[key] = value);
+
+		file = "parsedDataTransform2/conns-" + cellName + ".json"; // Individual cell export
+		jsonfile.writeFile(file, obj, (error) => { if (error) console.error(err) });	    
+	});
 })
 .catch(function(reason) {
 	console.log('promise rejected for ' + reason);
 });
+
+
+
